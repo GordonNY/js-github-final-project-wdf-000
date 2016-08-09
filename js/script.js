842e5d434ec979c7119dc82d5ca8a379ffcdf9ce
Gordon Gao <gordonsdev@gmail.com>
2016-08-09 18:30:13 +0000

    Done.

--- js/script.js
+++ js/script.js
@@ -1,64 +1,45 @@
-$(document).ready(function(){
-  var repoName, title, body;
-  submitForm();
-});
-
-function Issue(issueURL, title, body){
-  this.issueURL = issueURL;
-  this.title = title;
-  this.body = body;
-}
-
-Issue.prototype.renderIssue = function(selector){
-  var link = $('<a>')
-    .attr('href', this.issueURL)
-    .text(this.title);
-    selector.append(link);
-}
-
-function GithubInteractor(token){
-  this.token = token;
-  this.apiBaseUrl = "https://api.github.com/repos/";
-}
-
-var interactor = new GithubInteractor("YoudPutYourTokenHereInRealProduction")
 
 
-function submitForm(){
-  $('form').on('submit', function(event){
-    // take data below and turn into object with properties
+function submitForm() {
+  $('form').on('submit', (event) => {
     var repoName = $('#repoName').val();
     var repoOwner = $('#repoOwner').val();
     var title = $('#title').val();
     var body = $('#body').val();
+
     createIssue(repoName, repoOwner, title, body);
     event.preventDefault();
   });
 }
 
-function handleResponse(response) {
-  var issue = new Issue(response.html_url, response.title, response.body)
-  issue.renderIssue($('#issue'));
-}
-
-function handleError(jqXHR, textStatus, error) {
-  console.log("Post error: " + error);
-}
-
-function createIssue(repoName, repoOwner, title, body){
-  var url = interactor.apiBaseUrl + repoOwner + "/" + repoName + "/issues";
-  var data = {
+function createIssue(repoName, repoOwner, title, body) {
+  debugger;
+  // /repos/:owner/:repo/issues
+  const url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/issues';
+  const data = {
     title: title,
     body: body
-  }
+  };
+  // 
   $.ajax({
-    type: "POST",
     url: url,
+    type: 'POST',
+    dataType: 'json',
     beforeSend: function(xhr) {
-      xhr.setRequestHeader("Authorization", "token " + interactor.token);
+      xhr.setRequestHeader("Authorization", "token " + 'toke here');
     },
     data: JSON.stringify(data)
-  })
-  .done(handleResponse)
-  .fail(handleError);
+  }).done(function(response) {
+
+    var link = $('<a>').attr('href', this.issueURL).text(this.title);
+    $('#issue').append(link);
+
+  }).fail(function(error) {
+    // console.log(error);
+  });
 }
+
+
+$(document).ready(function(){
+  submitForm();
+});
